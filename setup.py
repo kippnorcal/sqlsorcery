6475932c602af6d03f0aac01e3da7786fd5d493f
dchess <dc.hess@gmail.com>
2019-08-28 19:48:45 -0700

    Fix version

--- setup.py
+++ setup.py
@@ -3,18 +3,20 @@ from os import path
 
 
 this_directory = path.abspath(path.dirname(__file__))
-with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+with open(path.join(this_directory, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
-setup(name='sqlsorcery',
-      version='0.3.1',
-      description='Dead simple wrapper for pandas and sqlalchemy',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      url='http://github.com/dchess/sqlsorcery',
-      author='dchess',
-      author_email='dc.hess@gmail.com',
-      license='MIT',
-      packages=['sqlsorcery'],
-      install_requires=['pandas', 'sqlalchemy', 'pyodbc', 'psycopg2-binary'],
-      zip_safe=False)
+setup(
+    name="sqlsorcery",
+    version="0.1.1",
+    description="Dead simple wrapper for pandas and sqlalchemy",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="http://github.com/dchess/sqlsorcery",
+    author="dchess",
+    author_email="dc.hess@gmail.com",
+    license="MIT",
+    packages=["sqlsorcery"],
+    install_requires=["pandas", "sqlalchemy", "pyodbc", "psycopg2-binary"],
+    zip_safe=False,
+)
