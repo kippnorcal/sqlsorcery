02bd5908b73dcf435dd535180f66eb17642838bd
dchess <dc.hess@gmail.com>
2019-10-17 21:02:12 -0700

    Update connection configs

--- sqlsorcery/__init__.py
+++ sqlsorcery/__init__.py
@@ -19,16 +19,6 @@ try:
 except ImportError:
     pyodbc = None
 
-try:
-    import cx_Oracle
-except ImportError:
-    cx_Oracle = None
-
-try:
-    import pymysql
-except ImportError:
-    pymysql = None
-
 
 class Connection:
     """Base class for sql connections containing shared class methods.
@@ -215,7 +205,16 @@ class MSSQL(Connection):
     """Child class that inherits from Connection with specific configuration
         for connecting to MS SQL."""
 
-    def __init__(self, schema=None, server=None, db=None, user=None, pwd=None):
+    def __init__(
+        self,
+        schema=None,
+        port=None,
+        server=None,
+        db=None,
+        user=None,
+        pwd=None,
+        bulk_insert=True,
+    ):
         """Initializes an MS SQL database connection
 
         .. note::
@@ -237,17 +236,45 @@ class MSSQL(Connection):
         :type pwd: string
         """
         self.server = server or getenv("MS_SERVER") or getenv("DB_SERVER")
+        self.port = port or getenv("MS_PORT") or getenv("DB_PORT")
         self.db = db or getenv("MS_DB") or getenv("DB")
         self.user = user or getenv("MS_USER") or getenv("DB_USER")
         self.pwd = pwd or getenv("MS_PWD") or getenv("DB_PWD")
         self.schema = schema or getenv("MS_SCHEMA") or getenv("DB_SCHEMA") or "dbo"
-        driver = f"{{{pyodbc.drivers()[0]}}}"
-        params = urllib.parse.quote_plus(
-            f"DRIVER={driver};SERVER={self.server};DATABASE={self.db};UID={self.user};PWD={self.pwd}"
-        )
-        self.engine = create_engine(
-            f"mssql+pyodbc:///?odbc_connect={params}", isolation_level="AUTOCOMMIT"
-        )
+        self.driver = pyodbc.drivers()[-1].replace(" ", "+")
+        cstr = f"mssql+pyodbc://{self.user}:{self.pwd}@{self.server}:{self.port}/{self.db}?driver={self.driver}"
+        self.engine = create_engine(cstr, fast_executemany=bulk_insert)
+
+
+class MySQL(Connection):
+    """Child class that inherits from Connection with specific configuration
+        for connecting to a MySQL  database."""
+
+    def __init__(self, server=None, port=None, db=None, user=None, pwd=None):
+        """Initializes a MySQL database connection
+
+        .. note::
+            When object is instantiated without params, SQLSorcery will
+            attempt to pull the values from the environment. See the
+            README for examples of setting these correctly in a .env
+            file.
+        :param server: IP or URL of database server
+        :type server: string
+        :param db: Name of database
+        :type db: string
+        :param user: Username for connecting to the database
+        :type user: string
+        :param pwd: Password for connecting to the database.
+            **Security Warning**: always pass this in with environment
+            variables when used in production.
+        :type pwd: string
+        """
+        self.server = server or getenv("MY_SERVER") or getenv("DB_SERVER")
+        self.db = db or getenv("MY_DB") or getenv("DB")
+        self.user = user or getenv("MY_USER") or getenv("DB_USER")
+        self.pwd = pwd or getenv("MY_PWD") or getenv("DB_PWD")
+        cstr = f"mysql+pymysql://{self.user}:{self.pwd}@{self.server}/{self.db}"
+        self.engine = create_engine(cstr)
 
 
 class PostgreSQL(Connection):
@@ -255,7 +282,14 @@ class PostgreSQL(Connection):
         for connecting to PostgreSQL."""
 
     def __init__(
-        self, schema=None, server=None, port=None, db=None, user=None, pwd=None
+        self,
+        schema=None,
+        server=None,
+        port=None,
+        db=None,
+        user=None,
+        pwd=None,
+        bulk_insert=True,
     ):
         """Initializes a PostgreSQL database connection
 
@@ -286,8 +320,8 @@ class PostgreSQL(Connection):
         self.pwd = pwd or getenv("PG_PWD") or getenv("DB_PWD")
         self.schema = schema or getenv("PG_SCHEMA") or getenv("DB_SCHEMA") or "public"
         sid = f"{self.server}:{self.port}/{self.db}"
-        cstr = f"postgres://{self.user}:{self.pwd}@{sid}"
-        self.engine = create_engine(cstr)
+        cstr = f"postgres+psycopg2://{self.user}:{self.pwd}@{sid}"
+        self.engine = create_engine(cstr, executemany_mode="batch")
 
 
 class Oracle(Connection):
@@ -325,14 +359,14 @@ class Oracle(Connection):
         self.sid = sid or getenv("OR_SID") or getenv("DB_SID")
         self.user = user or getenv("OR_USER") or getenv("DB_USER")
         self.pwd = pwd or getenv("OR_PWD") or getenv("DB_PWD")
-        sid = cx_Oracle.makedsn(self.server, self.port, sid=self.sid)
-        cstr = f"oracle://{self.user}:{self.pwd}@{sid}"
+        cstr = f"oracle+cx_oracle://{self.user}:{self.pwd}@{self.server}/{self.sid}"
         self.engine = create_engine(cstr)
 
 
 class SQLite(Connection):
     """Child class that inherits from Connection with specific configuration
         for connecting to a SQLite database file."""
+
     def __init__(self, path=None):
         """Initializes a SQLite  database connection
 
@@ -346,34 +380,4 @@ class SQLite(Connection):
         """
         cstr = f"sqlite:///{path}"
         self.schema = "main"
-        self.engine = create_engine(cstr)
-
-
-class MySQL(Connection):
-    """Child class that inherits from Connection with specific configuration
-        for connecting to a MySQL  database."""
-    def __init__(self, server=None, port=None, db=None, user=None, pwd=None):
-        """Initializes a MySQL database connection
-
-        .. note::
-            When object is instantiated without params, SQLSorcery will
-            attempt to pull the values from the environment. See the
-            README for examples of setting these correctly in a .env
-            file.
-        :param server: IP or URL of database server
-        :type server: string
-        :param db: Name of database
-        :type db: string
-        :param user: Username for connecting to the database
-        :type user: string
-        :param pwd: Password for connecting to the database.
-            **Security Warning**: always pass this in with environment
-            variables when used in production.
-        :type pwd: string
-        """
-        self.server = server or getenv("MY_SERVER") or getenv("DB_SERVER")
-        self.db = db or getenv("MY_DB") or getenv("DB")
-        self.user = user or getenv("MY_USER") or getenv("DB_USER")
-        self.pwd = pwd or getenv("MY_PWD") or getenv("DB_PWD")
-        cstr = f"mysql+pymysql://{self.user}:{self.pwd}@{self.server}/{self.db}"
         self.engine = create_engine(cstr)
